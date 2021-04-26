26c35a713c845ba2273fbdfd24c0409f55a34f40
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-26 20:05:49 -0300

    adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import',
+    'import-helpers',
+    'react-hooks',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'comma-spacing': 2,
+    'no-multiple-empty-lines': [2, { max: 1 }],
+    'no-trailing-spaces': 2,
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          '/^next/',
+          'module',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
