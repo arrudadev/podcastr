aa0f28b6b0b353992e846b6d51d36ce6f9a175bb
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-28 19:25:55 -0300

    adding PlayerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
+
 import { AppProps } from 'next/app';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList(episode);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        isPlaying,
+        play,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
