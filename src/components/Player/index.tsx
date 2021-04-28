b3b22659977f309caabbbb82c91ccc0d8b02c224
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-28 20:13:09 -0300

    adding rc-slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,6 +2,9 @@ import { useContext, useRef, useEffect } from 'react';
 
 import Image from 'next/image';
 
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
@@ -59,7 +62,15 @@ export function Player() {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
